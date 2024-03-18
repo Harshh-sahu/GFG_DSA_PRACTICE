ce4124f10286abacc04a390bc1eeaa96b7bfc63d
Harsh sahu <116263893+Harshh-sahu@users.noreply.github.com>
2024-03-18 19:55:02 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+          vector<long long int>ans;
+        for(int i=0;i<n;i++){
+            ans.push_back(arr1[i]);
+        }
+        for(int i=0;i<m;i++){
+            ans.push_back(arr2[i]);
+        }
+        sort(ans.begin(),ans.end());
+        return ans[k-1];
+    }
+};
+
+//{ Driver Code Starts.
+ 
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int n,m,k;
+		cin>>n>>m>>k;
+		int arr1[n],arr2[m];
+		for(int i=0;i<n;i++)
+			cin>>arr1[i];
+		for(int i=0;i<m;i++)
+			cin>>arr2[i];
+		
+		Solution ob;
+        cout << ob.kthElement(arr1, arr2, n, m, k)<<endl;
+	}
+    return 0;
+}
+// } Driver Code Ends
