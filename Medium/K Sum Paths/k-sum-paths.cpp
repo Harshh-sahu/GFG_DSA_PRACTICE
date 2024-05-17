66199e7b9a1aabfdb4b9416de2ee611f4b562c9a
Harsh sahu <116263893+Harshh-sahu@users.noreply.github.com>
2024-05-17 22:19:22 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/K Sum Paths/k-sum-paths.cpp
@@ -0,0 +1,154 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node 
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) 
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/*
+struct Node 
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+public:
+    int ans = 0; 
+
+    void pathFromOneRoot(Node* root, long long sum) {
+        if (!root) return;
+        
+        if (sum == root->data) {
+            ++ans;
+        }
+
+        pathFromOneRoot(root->left, sum - root->data);
+        pathFromOneRoot(root->right, sum - root->data);
+    }
+
+    void traverseAndCount(Node* root, int k) {
+        if (!root) return;
+
+        // Count paths starting from the current node
+        pathFromOneRoot(root, k);
+
+        // Traverse the left and right subtrees
+        traverseAndCount(root->left, k);
+        traverseAndCount(root->right, k);
+    }
+
+    int sumK(Node* root, int k) {
+        ans = 0; // Initialize the counter
+        traverseAndCount(root, k);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string tc;
+    getline(cin, tc);
+    int t = stoi(tc);
+  
+    while(t--)
+    {
+        string s ,ch;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        string key;
+        getline(cin, key);
+        int k=stoi(key);
+        Solution ob;
+        cout << ob.sumK(root, k) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
