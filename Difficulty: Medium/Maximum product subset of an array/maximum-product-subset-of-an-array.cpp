1e82ab950bbd3960b0663828d61d8086450da52a
Harsh sahu <116263893+Harshh-sahu@users.noreply.github.com>
2024-07-21 23:07:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum product subset of an array/maximum-product-subset-of-an-array.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    long long int findMaxProduct(vector<int>& arr) {
+        // Write your code here
+        if(arr.size()==1) return arr[0];
+        long long int prod=1;
+        int zero=0,non_zero=0,maxi=INT_MIN;
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]!=0){
+                prod=(prod*arr[i])%1000000007;
+                non_zero=1;
+            }
+            else{ zero=1;}
+            if(arr[i]<0 and arr[i]>maxi)
+            maxi=arr[i];
+        }
+        if(non_zero!=1) return 0;
+        if(arr.size()==2 and zero==1 and prod<0) return 0;
+        if(prod<0) prod/=maxi;
+        return prod;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        long long int ans = ob.findMaxProduct(arr);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
