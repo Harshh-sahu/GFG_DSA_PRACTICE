06eec4689afeb13b17aa92c31b663350cb538e61
Harsh sahu <harshsahu1143@gmail.com>
2024-09-26 19:54:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Roof Top/roof-top.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find maximum number of consecutive steps
+    // to gain an increase in altitude with each step.
+ int maxStep(vector<int>& arr) {
+        int cnt=0;
+        int ans=0;
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]>arr[i-1]) {
+                cnt++;
+            }else {
+                 ans=max(ans,cnt);
+                 cnt=0;
+            }
+        }
+        ans=max(ans,cnt);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        cout << ob.maxStep(arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
