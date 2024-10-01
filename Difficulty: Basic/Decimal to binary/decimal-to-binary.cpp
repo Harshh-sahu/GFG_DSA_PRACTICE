e6dd84173c7249f6845540062929e84bbd0e4058
Harsh sahu <harshsahu1143@gmail.com>
2024-10-01 16:32:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Decimal to binary/decimal-to-binary.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+void toBinary(int N)
+{
+    string ans="";
+   while(N>0){
+     ans = to_string(N%2)+ans;
+     N/=2;
+     
+   }
+   cout<<ans;
+ 
+        
+}
+
+//{ Driver Code Starts.
+
+int main() {
+	//code
+	
+	int t;
+	cin>>t;
+	
+	
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    toBinary(n);
+	    cout<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
