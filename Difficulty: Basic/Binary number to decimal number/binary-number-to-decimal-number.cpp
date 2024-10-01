25cb659f9799fd01cc6e011a1fe834c30e7490c0
Harsh sahu <harshsahu1143@gmail.com>
2024-10-01 16:29:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Binary number to decimal number/binary-number-to-decimal-number.cpp
@@ -0,0 +1,27 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int binary_to_decimal(string str) {
+
+   return stoi(str,nullptr,2);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        int ans = ob.binary_to_decimal(str);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
