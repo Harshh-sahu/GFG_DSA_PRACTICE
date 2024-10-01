a808764bf41b06536615994f91f2e25b37a06ae1
Harsh sahu <harshsahu1143@gmail.com>
2024-10-01 13:31:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Palindrome/palindrome.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+	public:
+		string is_palindrome(int n)
+		{
+		  string s1 = to_string(n);
+		  string s2 = to_string(n);
+		  
+		 reverse(s2.begin(),s2.end());
+		 if(s1 == s2)return "Yes";
+		 return "No";
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	Solution ob;
+    	string ans = ob.is_palindrome(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
