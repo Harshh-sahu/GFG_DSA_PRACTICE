1039bab796343c7d451ec5b815fc4d2d577eed33
Harsh sahu <harshsahu1143@gmail.com>
2025-01-09 14:34:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*You are required to complete this function*/
+
+class Solution {
+  public:
+    int maxLen(vector<int>& a) {
+        int maxLen = 0;
+    unordered_map<int, int> sumIndexMap;
+    int sum = 0;
+
+    for (int i = 0; i < a.size(); i++) {
+        sum += a[i];
+
+        if (sum == 0) {
+            maxLen = i + 1;
+        } else if (sumIndexMap.find(sum) != sumIndexMap.end()) {
+            maxLen = max(maxLen, i - sumIndexMap[sum]);
+        } else {
+            sumIndexMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+    while (t--) {
+        int n;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.maxLen(a) << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
