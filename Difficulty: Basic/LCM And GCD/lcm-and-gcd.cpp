f633b86f93943596da6066a0ac126a80c1abc9a5
Harsh sahu <harshsahu1143@gmail.com>
2025-02-10 10:55:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/LCM And GCD/lcm-and-gcd.cpp
+++ Difficulty: Basic/LCM And GCD/lcm-and-gcd.cpp
@@ -2,49 +2,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-#include <iostream>
-#include <vector>
 
+// } Driver Code Ends
 class Solution {
-public:
-    // Function to compute the GCD using the Euclidean algorithm
-    long long gcd(long long a, long long b) {
+  public:
+  int gcd(int a, int b) {
         while (b != 0) {
-            long long temp = b;
+            int temp = b;
             b = a % b;
             a = temp;
         }
         return a;
     }
 
-    // Function to compute the LCM using the relationship with GCD
-    long long lcm(long long num1, long long num2) {
-        return (num1 / gcd(num1, num2)) * num2; // Avoiding overflow by dividing first
-    }
+    // Function to compute LCM and GCD
+    std::vector<int> lcmAndGcd(int a, int b) {
+        int g = gcd(a, b);
+        int l = (a / g) * b;  // Using (a * b) / gcd(a, b) formula, avoiding overflow
 
-    // Function to return both LCM and GCD
-    std::vector<long long> lcmAndGcd(long long a, long long b) {
-        long long gc = gcd(a, b);
-        long long lc = lcm(a, b);
-        return {lc, gc}; // Returning LCM and GCD
+        return {l, g};
     }
-
 };
 
 //{ Driver Code Starts.
 int main() {
     int t;
     cin >> t;
+    cin.ignore();
     while (t--) {
-        long long A,B;
-        
-        cin>>A>>B;
+        int A, B;
+
+        cin >> A >> B;
 
         Solution ob;
-        vector<long long> ans = ob.lcmAndGcd(A,B);
-        cout<<ans[0]<<" "<<ans[1]<<endl;
+        vector<int> ans = ob.lcmAndGcd(A, B);
+        cout << ans[0] << " " << ans[1] << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
+
 // } Driver Code Ends
