116a43ada26baa24c3458e64fa64443f0a500617
Harsh sahu <harshsahu1143@gmail.com>
2025-05-02 13:04:12 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Transform to Sum Tree/transform-to-sum-tree.cpp
@@ -0,0 +1,153 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+void inorder(Node * node)
+{
+    if(node==NULL)
+        return;
+    
+    inorder(node->left);
+    cout<<node->data<<" ";
+    inorder(node->right);
+}
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+/* A binary tree node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+class Solution {
+public:
+    int convertToSumTree(Node* node) {
+        if (node == NULL)
+            return 0;
+
+        // Recursively get sum of left and right subtrees
+        int leftSum = convertToSumTree(node->left);
+        int rightSum = convertToSumTree(node->right);
+
+        // Store current node's value
+        int oldVal = node->data;
+
+        // Update current node's value to left + right sum
+        node->data = leftSum + rightSum;
+
+        // Return sum of subtree including original value of node
+        return node->data + oldVal;
+    }
+
+    void toSumTree(Node* node) {
+        convertToSumTree(node);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+		getline(cin,s);
+        Node* root = buildTree(s);
+        Solution ob;
+        ob.toSumTree(root);
+        inorder(root);
+        cout<<endl;
+    
+cout << "~" << "\n";
+}
+    return 1;
+}
+// } Driver Code Ends
