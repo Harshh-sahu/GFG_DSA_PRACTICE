7a08d210f094ef88c4faeb23e59597216a693bf7
Harsh sahu <harshsahu1143@gmail.com>
2025-06-21 09:00:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
@@ -1,85 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-#define MAX_HEIGHT 100000
-
-// Tree Node
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-};
-
-// Function to Build Tree
-Node* buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
-
-    // Create the root of the tree.....
-    Node* root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-
-// } Driver Code Ends
 /*
 struct Node {
     int data;
@@ -94,47 +12,39 @@ struct Node {
 */
 class Solution {
   public:
-     void tree(Node * root,vector<int>&ans){
-         if(root == NULL)return;
-         tree(root->left,ans);
-         ans.push_back(root->data);
-         tree(root->right,ans);
-         
-     }
+    // Function to return a list of integers denoting the node
+vector<int> merge(Node *root1, Node *root2) {
+    vector<int> ans;
 
-    vector<int> merge(Node *root1, Node *root2) {
-      vector<int>ans;
-      tree(root1,ans);
-      tree(root2,ans);
-      sort(ans.begin(),ans.end());
-      return ans;
-      
+    stack<Node*> sa, sb;
+    Node* a = root1;
+    Node* b = root2;
+
+    // Traverse both trees in-order simultaneously
+    while (a || b || !sa.empty() || !sb.empty()) {
+        // Go left in both trees as far as possible
+        while (a) {
+            sa.push(a);
+            a = a->left;
+        }
+        while (b) {
+            sb.push(b);
+            b = b->left;
+        }
+
+        // Decide whether to take element from tree A or tree B
+        if (sb.empty() || (!sa.empty() && sa.top()->data <= sb.top()->data)) {
+            Node* atop = sa.top(); sa.pop();
+            ans.push_back(atop->data);
+            a = atop->right; // move to right subtree
+        } else {
+            Node* btop = sb.top(); sb.pop();
+            ans.push_back(btop->data);
+            b = btop->right; // move to right subtree
+        }
     }
-};
 
-//{ Driver Code Starts.
-int main() {
-
-    int t;
-    string tc;
-    getline(cin, tc);
-    t = stoi(tc);
-    while (t--) {
-        string s;
-        getline(cin, s);
-        Node* root1 = buildTree(s);
-
-        getline(cin, s);
-        Node* root2 = buildTree(s);
-
-        // getline(cin, s);
-        Solution obj;
-        vector<int> vec = obj.merge(root1, root2);
-        for (int i = 0; i < vec.size(); i++)
-            cout << vec[i] << " ";
-        cout << endl;
-        /// cout<<"~"<<endl;
-    }
-    return 0;
+    return ans;
 }
-// } Driver Code Ends
+
+};
